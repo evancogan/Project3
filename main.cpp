41aeb89dda296a680fc33a9d2d322ce6338498c0
Evan J. Cogan <evan.james.programming@gmail.com>
2026-06-21 04:14:47 +0000

    Finished a rough draft of the entire program

--- main.cpp
+++ main.cpp
@@ -40,108 +40,99 @@ class Menu {
 
 // The CornerGrocerApp class talks to the Menu class and handles the data processing, sending it back to the Menu class for display.
 class CornerGrocerApp {
-    private:
-        std::map<std::string, int> itemFrequencies;
-        std::string inputFileName = "CS210_Project_Three_Input_File.txt";
-        std::string backupFileName = "frequency.dat";
-        Menu menu;
+private:
+    std::map<std::string, int> itemFrequencies;
+    std::string inputFileName = "CS210_Project_Three_Input_File.txt";
+    std::string backupFileName = "frequency.dat";
+    Menu menu;
 
-    public:
-        void loadData() {
-            std::ifstream inputFile(inputFileName);
+    bool loadData() {
+        std::ifstream inputFile(inputFileName);
 
-            if (!inputFile) {
-                std::cerr << "Could not open file!" << inputFileName << std::endl;
-                return;
-            }
-
-            std::string item;
-            while (std::getline(inputFile, item)) {
-                itemFrequencies[item]++;
-            }
-
-            inputFile.close();
+        if (!inputFile) {
+            std::cerr << "Could not open file: " << inputFileName << std::endl;
+            return false;
         }
 
-        void saveFrequencyBackup() {
-            std::ofstream outputFile(backupFileName);
-
-            if (!outputFile) {
-                std::cerr << "Could not create backup file: " << backupFileName << std::endl;
-                return;
-            }
-
-            for (const auto& pair : itemFrequencies) {
-                outputFile << pair.first << " " << pair.second << std::endl;
-            }
-
-            outputFile.close();
+        std::string item;
+        while (inputFile >> item) {
+            itemFrequencies[item]++;
         }
 
-        void run() {
-            saveFrequencyBackup();
+        return true;
+    }
 
-            if (!loadData()) {
-                std::cerr << "Failed to load data from file." << std::endl;
-                return;
-            }
+    void saveFrequencyBackup() {
+        std::ofstream outputFile(backupFileName);
 
-            saveFrequencyBackup();
-            
-            do {
-                menu.displayMenu();
-                int choice = menu.getChoice();
-
-                switch (choice) {
-                    case 1:
-                        searchItem();
-                        break;
-                    case 2:
-                        printFrequencies();
-                        break;
-                    case 3:
-                        printHistogram();
-                        break;
-                    case 4:
-                        std::cout << "Exiting program. Goodbye!" << std::endl;
-                        break;
-                }
-            } while (choice != 4);
-
-    private:
-        loadData() {
-            std::ifstream inputFile(inputFileName);
-
-            if (!inputFile) {
-                std::cerr << "Could not open file!" << inputFileName << std::endl;
-                return false;
-            }
-
-            std::string item;
-            while (std::getline(inputFile, item)) {
-                itemFrequencies[item]++;
-            }
-
-            inputFile.close();
-            return true;
+        if (!outputFile) {
+            std::cerr << "Could not create backup file: " << backupFileName << std::endl;
+            return;
         }
 
-        saveFrequencyBackup() {
-            std::ofstream outputFile(backupFileName);
-
-            if (!outputFile) {
-                std::cerr << "Could not create backup file: " << backupFileName << std::endl;
-                return;
-            }
-
-            for 
-                
-            
+        for (const auto& pair : itemFrequencies) {
+            outputFile << pair.first << " " << pair.second << std::endl;
         }
+    }
+
+    void searchItemFrequency() {
+        std::cout << "Enter item name to search: ";
+        std::string itemName;
+        std::cin >> itemName;
+        auto it = itemFrequencies.find(itemName);
+        if (it != itemFrequencies.end()) {
+            std::cout << "Frequency of " << itemName << ": " << it->second << std::endl;
+        } else {
+            std::cout << itemName << " not found!" << std::endl;
+        }
+    }
+
+    void printAllFrequencies() {
+        for (const auto& pair : itemFrequencies) {  // Here we use a key-value pair to iterate through the map and print each item and its frequency.
+            std::cout << pair.first << ": " << pair.second << std::endl;
+        }
+    }
+
+    void printHistogram() {
+        for (const auto& pair : itemFrequencies) {
+            std::cout << pair.first << ": " << std::string(pair.second, '*') << std::endl;
+        }
+    }
+// The run method is the main loop of the application, handling user input and calling the appropriate methods based on the user's choice.
+public:
+    void run() {
+        if (!loadData()) {
+            std::cerr << "Failed to load data from file." << std::endl;
+            return;
+        }
+
+        saveFrequencyBackup();
+
+        int choice;
+        do {
+            menu.displayMenu();
+            choice = menu.getChoice();
+
+            switch (choice) {
+                case 1:
+                    searchItemFrequency();
+                    break;
+                case 2:
+                    printAllFrequencies();
+                    break;
+                case 3:
+                    printHistogram();
+                    break;
+                case 4:
+                    std::cout << "Exiting program. Goodbye!" << std::endl;
+                    break;
+            }
+        } while (choice != 4);
+    }
 };
-
 int main() {
-    std::cout << "Hello, World!\n";
+    CornerGrocerApp app;
+    app.run();
     return 0;
 }
 
